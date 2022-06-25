f1413d873a1ca5656b06b586e1a50b127e0fd7e5
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-06-25 14:50:42 +0100

    feat:Add models, views and url to our blog page

--- I4G0028685VH/blog/views.py
+++ I4G0028685VH/blog/views.py
@@ -1,38 +1,42 @@
 from audioop import reverse
 from dataclasses import fields
 from msilib.schema import ListView
+from re import template
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-from django.views.generic.edit import DetailView
+from django.views.generic import DetailView
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
-
-
 from I4G0028685VH.blog.models import Post
 
 # Create your views here.
 class PostListView(ListView):
     model = Post
+    template_name = 'post_list.html'
 
 
 class PostCreateView(CreateView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
+    template_name = 'post_form.html'
 
 
 class PostDetailView(DetailView):
     model = Post
+    template_name = 'post_detail.html'
 
 
 class PostUpdateView(UpdateView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all") 
+    template_name = 'base.html'
 
 
 class PostDeleteView(DeleteView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
+    template_name = 'post_confirm_delete.html'
