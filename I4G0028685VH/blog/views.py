8d0444f21515a2809fbde6ab6505f9869041036b
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-06-27 05:12:51 +0100

    chore:Work on the error and everything working fine

--- I4G0028685VH/blog/views.py
+++ I4G0028685VH/blog/views.py
@@ -5,38 +5,44 @@ from re import template
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
-from I4G0028685VH.blog.models import Post
+from django.views.generic.list import ListView
+
+
+from .models import Post
 
 # Create your views here.
 class PostListView(ListView):
     model = Post
-    template_name = 'post_list.html'
+    def get_queryset(self):
+        return super().get_queryset().filter(status="published")
+
+    # template_name = 'blog/post_list.html'
 
 
 class PostCreateView(CreateView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
-    template_name = 'post_form.html'
+    # template_name = 'blog/post_form.html'
 
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    # template_name = 'blog/post_detail.html'
 
 
 class PostUpdateView(UpdateView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all") 
-    template_name = 'base.html'
+    # template_name = 'base.html'
 
 
 class PostDeleteView(DeleteView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
-    template_name = 'post_confirm_delete.html'
+    # template_name = 'blog/post_confirm_delete.html'
